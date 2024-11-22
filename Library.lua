123ad8b70e4ac539a4f39a817581420f1248fd6d
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-11-22 20:21:36 -0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -55,7 +55,7 @@ tempestButton.TextSize = 14.000
 tempestButton.TextWrapped = true
 
 tempestButton.Activated:Connect(function()
-	ScreenGui.Frame.Visible = not ScreenGui.Frame.Visible
+	backgroundFrame.Visible = not backgroundFrame.Visible
 end)
 
 local Toggles = {}
@@ -3208,7 +3208,6 @@ function Library:CreateWindow(...)
 		Size = Config.Size,
 		Visible = false,
 		ZIndex = 1,
-		Name = "backgroundPrincipalFrame",
 		Parent = ScreenGui,
 	})
 

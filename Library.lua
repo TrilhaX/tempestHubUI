52adff711f4f6836bf1b9f545d9bfa2f1625f844
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-11-25 20:30:41 -0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -17,6 +17,7 @@ local Mouse = LocalPlayer:GetMouse()
 local ProtectGui = protectgui or (syn and syn.protect_gui) or function() end
 
 local ScreenGui = Instance.new('ScreenGui')
+ScreenGui.Name = "LinoriaGui"
 ProtectGui(ScreenGui)
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global
 if gethui then
